0e8981ec138cc5288efb92bc541eb1944a3a4233
Jan Blechta <blechta@karlin.mff.cuni.cz>
2014-07-17 18:16:08 +0200

    Initial commit.
    PCD preconditioner for linearized Navier-Stokes in place. Now just
    with Cholesky on Mp^{-1}, Ap^{-1}.

---
+++ demo/demo_stokes-mini.py
@@ -0,0 +1,117 @@
+from dolfin import *
+from field_split import FieldSplitSolver
+
+import sys
+
+try:
+    num_refinements = int(sys.argv[1])
+except IndexError:
+    num_refinements = 0
+
+# Load mesh and subdomains
+mesh = Mesh("dolfin_fine.xml.gz")
+sub_domains = MeshFunction("size_t", mesh, "dolfin_fine_subdomains.xml.gz")
+
+# Refine
+# NOTE: Only works sequentially
+sub_domains_old = []
+for i in range(num_refinements):
+    mesh = adapt(mesh, CellFunction('bool', mesh, True))
+    # BUG in DOLFIN: this is needed to prevent memory corruption
+    sub_domains_old.append(sub_domains)
+    sub_domains = adapt(sub_domains, mesh)
+
+# Define function spaces
+P1 = FunctionSpace(mesh, "Lagrange", 1)
+B  = FunctionSpace(mesh, "Bubble", 3)
+gdim = mesh.geometry().dim()
+V  = MixedFunctionSpace(gdim*(P1+B,))
+Q  = FunctionSpace(mesh, "CG",  1)
+Mini = MixedFunctionSpace((V, Q))
+info('Dimension of the function space %g' % Mini.dim())
+
+# No-slip boundary condition for velocity
+noslip = Constant((0.0, 0.0))
+bc0 = DirichletBC(Mini.sub(0), noslip, sub_domains, 0)
+
+# Inflow boundary condition for velocity
+inflow = Expression(("-sin(x[1]*pi)", "0.0"))
+bc1 = DirichletBC(Mini.sub(0), inflow, sub_domains, 1)
+
+# Boundary condition for pressure at outflow
+zero = Constant(0.0)
+bc2 = DirichletBC(Mini.sub(1), zero, sub_domains, 2)
+
+# Collect boundary conditions
+bcs = [bc0, bc1, bc2]
+
+# Define variational problem
+(u, p) = TrialFunctions(Mini)
+(v, q) = TestFunctions(Mini)
+#nu = Constant(1e-1)
+nu = Expression('x[1] < x1 ? nu1 : nu2', x1=0.5, nu1=1e1, nu2=2e-1)
+f  = Constant((0.0, 0.0))
+a  = (nu*inner(grad(u), grad(v)) + div(v)*p + q*div(u))*dx
+L  = inner(f, v)*dx
+
+# Add Oseen-like convection
+u0 = Function(V)
+a += inner(dot(grad(u), u0), v)*dx
+
+# Operators for PCD preconditioner
+Mp = p*q*dx
+Fp = (nu*inner(grad(p), grad(q)) + dot(grad(p), u0)*q)*dx
+Ap = inner(grad(p), grad(q))*dx
+Lp = Constant(0.0)*q*dx # Dummy right-hand side
+
+# Assemble
+A, b = assemble_system(a, L, bcs)
+
+# Setup fieldsplit solver
+solver = FieldSplitSolver(Mini)
+solver.parameters["monitor_convergence"] = True
+solver.parameters["relative_tolerance"] = 1e-6
+solver.parameters["maximum_iterations"] = 100
+solver.parameters["nonzero_initial_guess"] = True
+solver.parameters["error_on_nonconvergence"] = False
+solver.parameters['gmres']['restart'] = 100
+
+# AMG approximation to 0,0-block inverse
+#PETScOptions.set("fieldsplit_u_ksp_type", "richardson")
+##PETScOptions.set("fieldsplit_u_pc_type", "gamg") # Does not work
+#PETScOptions.set("fieldsplit_u_pc_type", "ml")
+#PETScOptions.set("fieldsplit_u_ksp_max_it", 1)
+#PETScOptions.set("fieldsplit_u_mg_levels_ksp_type", "chebyshev")
+#PETScOptions.set("fieldsplit_u_mg_levels_ksp_max_it", 2)
+#PETScOptions.set("fieldsplit_u_mg_levels_pc_type", "sor")
+#PETScOptions.set("fieldsplit_u_mg_levels_pc_sor_its", 2)
+
+# LU 0,0-block inverse
+PETScOptions.set("fieldsplit_u_ksp_type", "preonly")
+PETScOptions.set("fieldsplit_u_pc_type", "lu")
+PETScOptions.set("fieldsplit_u_pc_factor_mat_solver_package", "mumps")
+
+# Compute solution using Ossen approximation
+w = Function(Mini)
+solver.set_operator(A)
+solver.setup(Mp, Fp, Ap, Lp, [bc2])
+solver.solve(w.vector(), b)
+while True:
+    u0.assign(w.sub(0, deepcopy=True))
+    A, b = assemble_system(a, L, bcs)
+    solver.set_operator(A)
+    # End when num iterations is zero (residual is small)
+    if solver.solve(w.vector(), b) == 0:
+        break
+
+# Split the mixed solution using a shallow copy
+(u, p) = w.split()
+
+# Save solution in VTK format
+File("velocity.xdmf") << u
+File("pressure.xdmf") << p
+
+# Plot solution
+plot(u)
+plot(p)
+interactive()
---
+++ src/field_split.py
@@ -0,0 +1,143 @@
+from dolfin import (PETScKrylovSolver, compile_extension_module,
+        as_backend_type, PETScMatrix, SystemAssembler)
+from petsc4py import PETSc
+
+class FieldSplitSolver(PETScKrylovSolver):
+    def __init__(self, space):
+        # Setup GMRES with RIGHT preconditioning
+        ksp = PETSc.KSP()
+        ksp.create(PETSc.COMM_WORLD)
+        ksp.setType(PETSc.KSP.Type.GMRES)
+        ksp.setPCSide(PETSc.PC.Side.RIGHT)
+        self._ksp = ksp
+
+        # Setup SCHUR with UPPER factorization
+        pc = ksp.getPC()
+        pc.setType(PETSc.PC.Type.FIELDSPLIT)
+        pc.setFieldSplitType(PETSc.PC.CompositeType.SCHUR)
+        pc.setFieldSplitSchurFactType(PETSc.PC.SchurFactType.UPPER)
+        is0 = dofmap_dofs_is(space.sub(0).dofmap())
+        is1 = dofmap_dofs_is(space.sub(1).dofmap())
+        pc.setFieldSplitIS(['u', is0], ['p', is1])
+        is0.destroy()
+        self._is1 = is1 # Will be needed by Schur PC
+
+        # Init mother class
+        PETScKrylovSolver.__init__(self, ksp)
+
+    # Override PETScKrylovSolver::set_operator() method
+    def set_operator(self, A):
+        PETScKrylovSolver.set_operator(self, A)
+        self._ksp.setOperators(as_backend_type(A).mat())
+
+    # Discard PETScKrylovSolver::set_operators() method
+    def set_operators(self, *args):
+        raise NotImplementedError
+
+    def setup(self, *args):
+        # Setup KSP and PC
+        ksp = self._ksp
+        ksp.setUp()
+        pc = ksp.getPC()
+        pc.setUp()
+
+        # Get sub-KSPs, sub-PCs
+        ksp0, ksp1 = pc.getFieldSplitSubKSP()
+        pc0, pc1 = ksp0.getPC(), ksp1.getPC()
+
+        # Setup approximation of 0,0-block inverse
+        ksp0.setFromOptions()
+        pc0.setFromOptions()
+
+        # Setup approximation of Schur complement inverse
+        ksp1.setType(PETSc.KSP.Type.PREONLY)
+        pc1.setType(PETSc.PC.Type.PYTHON)
+        pc1.setPythonContext(PCD_preconditioner(self._is1, *args))
+        self._is1.destroy()
+
+
+class PCD_preconditioner(object):
+    def __init__(self, *args):
+        if args:
+            self.set_operators(*args)
+            self.setup()
+    def setup(self):
+        self._mp = PETScMatrix()
+        self._fp = PETScMatrix()
+        self._ap = PETScMatrix()
+        # TODO: What are correct BCs?
+        assembler = SystemAssembler(self._Mp, self._Lp, self._bcs_Ap)
+        assembler.assemble(self._mp)
+        assembler = SystemAssembler(self._Fp, self._Lp, self._bcs_Ap)
+        assembler.assemble(self._fp)
+        assembler = SystemAssembler(self._Ap, self._Lp, self._bcs_Ap)
+        assembler.assemble(self._ap)
+        self._mp = self._mp.mat().getSubMatrix(self._isp, self._isp)
+        self._ap = self._ap.mat().getSubMatrix(self._isp, self._isp)
+        self._fp = self._fp.mat().getSubMatrix(self._isp, self._isp)
+        self.prepare_factors()
+    def apply(self, pc, x, y):
+        # y = S^{-1} x = M_p^{-1} F_p  A_p^{-1} x
+        self._ksp_ap.solve(x, y)
+        # TODO: Try matrix-free!
+        # TODO: Is modification of x safe?
+        self._fp.mult(y, x)
+        self._ksp_mp.solve(x, y)
+    def set_operators(self, isp, Mp, Fp, Ap, Lp, bcs_Ap):
+        self._isp = isp
+        self._Mp = Mp
+        self._Fp = Fp
+        self._Ap = Ap
+        self._Lp = Lp
+        self._bcs_Ap = bcs_Ap
+    def prepare_factors(self):
+        # Prepare Mp factorization
+        ksp = PETSc.KSP()
+        ksp.create(PETSc.COMM_WORLD)
+        ksp.setType(PETSc.KSP.Type.PREONLY)
+        pc = ksp.getPC()
+        pc.setType(PETSc.PC.Type.CHOLESKY)
+        pc.setFactorSolverPackage('mumps')
+        self._mp.setOption(PETSc.Mat.Option.SPD, True)
+        ksp.setOperators(self._mp)
+        ksp.setUp()
+        self._ksp_mp = ksp
+
+        # Prepare Ap factorization
+        ksp = PETSc.KSP()
+        ksp.create(PETSc.COMM_WORLD)
+        ksp.setType(PETSc.KSP.Type.PREONLY)
+        pc = ksp.getPC()
+        pc.setType(PETSc.PC.Type.CHOLESKY)
+        pc.setFactorSolverPackage('mumps')
+        self._mp.setOption(PETSc.Mat.Option.SPD, True)
+        ksp.setOperators(self._ap)
+        ksp.setUp()
+        self._ksp_ap = ksp
+
+
+dofmap_dofs_is_cpp_code = """
+#ifdef SWIG
+%include "petsc4py/petsc4py.i"
+#endif
+
+#include <petsc.h>
+#include <dolfin/fem/GenericDofMap.h>
+
+namespace dolfin {
+
+  IS dofmap_dofs_is(const GenericDofMap& dofmap)
+  {
+    const std::vector<dolfin::la_index> dofs = dofmap.dofs();
+    IS is;
+    ISCreateGeneral(PETSC_COMM_WORLD, dofs.size(), dofs.data(),
+                    PETSC_COPY_VALUES, &is);
+    return is;
+  }
+
+}
+"""
+
+dofmap_dofs_is = \
+    compile_extension_module(dofmap_dofs_is_cpp_code).dofmap_dofs_is
+del dofmap_dofs_is_cpp_code
