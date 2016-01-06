Start polishing the fixes in parallel PCD BC computation.

fenapack/preconditioners.py
@@ -17,7 +17,6 @@
 
 import dolfin
 from petsc4py import PETSc
-import numpy as np
 
 from fenapack.field_split import test_subfield_bc
 
@@ -192,20 +191,16 @@ class PCDPC_BMR(BasePCDPC):
         """
         timer = dolfin.Timer("FENaPack: call PCDPC_BMR.apply")
         timer.start()
-        x.assemble() # FIXME: Why is this needed?!
+        #x.assemble() # FIXME: Why is this NOT needed?!
         x0 = x.copy()
-        #x.setValues(self._bc_indices, self._bc_values) # Apply bcs to rhs
-        rank = dolfin.MPI.rank(dolfin.mpi_comm_world())
-        #print rank, x.owner_range
-        #print rank, x.owner_ranges
-        #print rank, self._is1.local_size
-        #print rank, len(self._Ap_bc.function_space().dofmap().dofs())
-        #print rank, self._Ap_bc.function_space().dofmap().dofs()
-        test_subfield_bc(x, self._Ap_bc, self._Ap_bc.function_space().dofmap(),
-                         self._is1)
-        #import pdb; pdb.set_trace()
-        #exit()
-        x.assemble() # FIXME: Why is this needed?!
+        # Apply bcs to rhs
+        # FIXME: Shouldn't we treat list of boundary conditions at once
+        #        within the C++ layer?
+        for bc in self._bcs:
+            test_subfield_bc(x, bc, bc.function_space().dofmap(), self._is1)
+        # NOTE: The following routine should be called after completing all
+        #       calls to 'VecSetValues()'.
+        x.assemble() # FIXME: Move this to routine which calls 'VecSetValues()'
         self._ksp_Ap.solve(x, y) # y = A_p^{-1} x
         self._Kp.mult(-y, x)
         x.axpy(-self._nu, x0)    # x = -K_p y - nu*x0
@@ -228,28 +223,8 @@ class PCDPC_BMR(BasePCDPC):
             # Make sure that 'bcs' is a list
             if not isinstance(bcs, list):
                 bcs = [bcs]
-            assert len(bcs) == 1
-            self._Ap_bc = bcs[0]
+            self._bcs = bcs
             self._is1 = is1
-            ## Get indices and values
-            ## FIXME: Avoid dictionaries!!!
-            #indices = []
-            #values = []
-            #for bc in bcs:
-            #    bc_map = bc.get_boundary_values()
-            #    indices += bc_map.keys()
-            #    values += bc_map.values()
-            ## Get boundary indices for 11-block
-            #lgmap = PETSc.LGMap().createIS(is1)
-            ## FIXME: This does not work in parallel!
-            ##self._bc_indices = lgmap.applyInverse(indices, PETSc.LGMap.MapType.DROP).tolist()
-            #self._bc_indices = lgmap.applyInverse(indices, PETSc.LGMap.MapType.MASK)#.tolist()
-            #self._bc_values = np.array(values)
-            ##import pdb; pdb.set_trace()
-            #rank = dolfin.MPI.rank(dolfin.mpi_comm_world())
-            #print rank, self._bc_indices
-            #self._bc_values = self._bc_values[self._bc_indices>=0]
-            #self._bc_indices = self._bc_indices[self._bc_indices>=0]
             # Update flag
             self._isset_bc = True
         elif not self._isset_bc: