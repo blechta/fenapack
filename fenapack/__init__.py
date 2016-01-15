32ac151b8cdd35f99bddf27a27dc1886c76d93b4
Martin Rehor <rehor@karlin.mff.cuni.cz>
2016-01-15 10:57:34 +0100

    Start using UFLACS within FENaPack.

--- fenapack/__init__.py
+++ fenapack/__init__.py
@@ -1,3 +1,9 @@
+# Set up DOLFIN's default global parameters
+from dolfin import parameters
+parameters["form_compiler"]["representation"] = "uflacs"
+parameters["form_compiler"]["optimize"] = True
+
+# Import FENaPack modules
 from fenapack.field_split import *
 from fenapack.preconditioners import *
 from fenapack.nonlinear_solvers import *
