19c6e465aa6122f7b6f2bb453f141a8867340027
Jan Blechta <blechta@karlin.mff.cuni.cz>
2016-05-13 10:38:13 +0200

    Update regression test launcher to work with FENaPack

--- test/regression/test.py
+++ test/regression/test.py
@@ -1,22 +1,22 @@
 """Run all demos."""
 
-# Copyright (C) 2008-2014 Ilmar Wilbers
+# Copyright (C) 2008-2014 Ilmar Wilbers, 2016 Jan Blechta
 #
-# This file is part of DOLFIN.
+# This file is part of FENaPack based on the file from DOLFIN.
 #
-# DOLFIN is free software: you can redistribute it and/or modify
+# FENaPack is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# DOLFIN is distributed in the hope that it will be useful,
+# FENaPack is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-#
+# along with FENaPack.  If not, see <http://www.gnu.org/licenses/>.
+
 # Modified by Anders Logg, 2008-2009.
 # Modified by Johannes Ring, 2009, 2011-2012.
 # Modified by Johan Hake, 2009.
@@ -38,6 +38,10 @@ def get_executable_name(demo, lang) :
      directories under demo/[undocumented,documented] and lang (cpp or py)
      should be excluded.
   """
+  # FENaPack demodir structure
+  if lang == 'python' and demo[-3:] == '.py':
+      return demo[:-3]
+
   directories = demo.split(os.path.sep)
 
   # Search for "demo" and lang from right
@@ -77,10 +81,11 @@ def run_python_demo(prefix, demo, rootdir, timing, failed):
     print("Running Python demo %s%s" % (prefix, demo))
     print("")
 
+    demodir = demo if os.path.isdir(demo) else os.path.dirname(demo)
     demofile = get_executable_name(demo, "python") + '.py'
 
     t1 = time()
-    os.chdir(demo)
+    os.chdir(demodir)
     status, output = get_status_output("%s %s -u %s" % (prefix, sys.executable, demofile))
     os.chdir(rootdir)
     t2 = time()
@@ -111,21 +116,7 @@ def main():
     # NOTE: Demo must be listed here iff unimplemented otherwse the test will
     #       fail. This is meant to protect against usual bad named demos not
     #       executed for ages in regression tests.
-    not_implemented = \
-      [os.path.join(demodir, 'undocumented', 'projection-interpolation',    'cpp'),
-       os.path.join(demodir, 'undocumented', 'interpolation',               'cpp'),
-       os.path.join(demodir, 'undocumented', 'adaptive-poisson',            'cpp'),
-       os.path.join(demodir, 'undocumented', 'multistage-solver',           'cpp'),
-       os.path.join(demodir, 'undocumented', 'smoothing',                   'cpp'),
-       os.path.join(demodir, 'undocumented', 'overlapping-regions',         'cpp'),
-       os.path.join(demodir, 'undocumented', 'sub-function-assignment',     'cpp'),
-       os.path.join(demodir, 'undocumented', 'compiled-extension-module',   'cpp'),
-       os.path.join(demodir, 'undocumented', 'timing',                      'cpp'),
-       os.path.join(demodir, 'undocumented', 'mplot',                       'cpp'),
-       os.path.join(demodir, 'documented',   'stokes-mini',                 'cpp'),
-       os.path.join(demodir, 'documented',   'tensor-weighted-poisson',     'cpp'),
-       os.path.join(demodir, 'documented',   'subdomains-poisson',          'cpp'),
-       ]
+    not_implemented = []
 
     # Demos to run
     cppdemos = []
@@ -148,7 +139,11 @@ def main():
             else:
                 assert dpath in not_implemented, \
                     "Non-existing demo '%s' not marked as not_implemented" % dpath
-    print(pydemos)
+        # This is the codepath currently followed in FENaPack
+        else:
+            for f in fnames:
+                if len(f) > 3 and f[-3:] == '.py':
+                    pydemos.append(os.path.join(dpath, f))
 
     # Set non-interactive
     os.putenv('DOLFIN_NOPLOT', '1')
@@ -173,36 +168,7 @@ def main():
             cppdemos.append(s)
 
     # List of demos that throw expected errors in parallel
-    not_working_in_parallel = \
-      [os.path.join(demodir, 'undocumented', 'adaptive-poisson',            'python'),
-       os.path.join(demodir, 'undocumented', 'auto-adaptive-navier-stokes', 'cpp'),
-       os.path.join(demodir, 'undocumented', 'auto-adaptive-navier-stokes', 'python'),
-       os.path.join(demodir, 'documented',   'auto-adaptive-poisson',       'cpp'),
-       os.path.join(demodir, 'documented',   'auto-adaptive-poisson',       'python'),
-       os.path.join(demodir, 'undocumented', 'eval',                        'cpp'),
-       os.path.join(demodir, 'undocumented', 'eval',                        'python'),
-       os.path.join(demodir, 'undocumented', 'extrapolation',               'cpp'),
-       os.path.join(demodir, 'undocumented', 'extrapolation',               'python'),
-       os.path.join(demodir, 'undocumented', 'meshfunction-refinement',     'cpp'),
-       os.path.join(demodir, 'undocumented', 'nonmatching-interpolation',   'cpp'),
-       os.path.join(demodir, 'undocumented', 'nonmatching-interpolation',   'python'),
-       os.path.join(demodir, 'undocumented', 'nonmatching-projection',      'cpp'),
-       os.path.join(demodir, 'undocumented', 'nonmatching-projection',      'python'),
-       os.path.join(demodir, 'undocumented', 'poisson-disc',                'python'),
-       os.path.join(demodir, 'undocumented', 'poisson-disc',                'cpp'),
-       os.path.join(demodir, 'undocumented', 'smoothing',                   'python'),
-       os.path.join(demodir, 'documented',   'subdomains',                  'cpp'),
-       os.path.join(demodir, 'documented',   'subdomains',                  'python'),
-       os.path.join(demodir, 'undocumented', 'submesh',                     'cpp'),
-       os.path.join(demodir, 'undocumented', 'submesh',                     'python'),
-       os.path.join(demodir, 'undocumented', 'time-series',                 'cpp'),
-       os.path.join(demodir, 'undocumented', 'time-series',                 'python'),
-       os.path.join(demodir, 'undocumented', 'triangulate',                 'cpp'),
-       os.path.join(demodir, 'undocumented', 'triangulate',                 'python'),
-       os.path.join(demodir, 'undocumented', 'poisson1D-in-2D',             'cpp'),
-       os.path.join(demodir, 'undocumented', 'poisson1D-in-2D',             'python'),
-       os.path.join(demodir, 'undocumented', 'compiled-extension-module',   'python'),
-       ]
+    not_working_in_parallel = []
 
     failed = []
     timing = []
