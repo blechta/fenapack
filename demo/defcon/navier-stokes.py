3c34512bb4646a28d72df89e6a3085a256eab225
Jan Blechta <blechta@karlin.mff.cuni.cz>
2017-03-08 22:49:21 +0000

    Acknowledge Patrick's authorship of defcon demo [skip ci]

--- demo/defcon/navier-stokes.py
+++ demo/defcon/navier-stokes.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2016-2017 Patrick Farrell, Jan Blechta
+#
+# This file comes originally from defcon.
+# This file is part of FENaPack.
+#
+# FENaPack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FENaPack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with FENaPack.  If not, see <http://www.gnu.org/licenses/>.
+
 # Let PETSc print memory usage at the end
 import petsc4py
 petsc4py.init(("python", "-malloc_info"))
